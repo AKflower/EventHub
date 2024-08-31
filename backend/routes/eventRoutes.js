0d337ff847fae0344eacaba1a42e47ad4af5cb40
Trần Quốc Dũng <78058950+danisolation@users.noreply.github.com>
2024-08-31 11:15:15 +0700

    [Feat]

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -6,8 +6,9 @@ const eventController = require('../controllers/eventController');
 // Các endpoint liên quan đến sự kiện
 router.post('/events', eventController.createEvent);
 router.get('/events', eventController.getAllEvents);
-router.get('/events/:id', eventController.getEventById);
 router.get('/events/filter', eventController.getEventsByCategoryAndIsFree);
+
+router.get('/events/:id', eventController.getEventById);
 router.get('/search-by-name', eventController.searchEventsByName);
 router.put('/events/:id', eventController.updateEvent);
 router.put('/events/:id/soft-delete', eventController.softDeleteEvent);
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3001;
-const cors = require('cors')
 
 const db = require("./db");
 const userRoutes = require("./routes/userRoutes");
