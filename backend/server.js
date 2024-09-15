5e4f1a0004b2d22badc3e9bbbfcc8ce6e685a017
Trần Quốc Dũng <78058950+danisolation@users.noreply.github.com>
2024-09-16 02:04:41 +0700

    [Feat]

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const db = require("./db");
 const cron = require("node-cron");
 const moment = require('moment')
+const nodemailer = require("nodemailer");
 
 const userRoutes = require("./routes/userRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
@@ -63,6 +64,50 @@ const updateEventStatus = async () => {
 };
 
 cron.schedule('*/1 * * * *', updateEventStatus);
+
+cron.schedule('0 0 * * *', async () => {
+  try {
+    const result = await db.query(`
+      SELECT e.name, e."startTime", b.mail
+      FROM events e
+      JOIN bookings b ON e.id = b."eventId"
+      WHERE e."isDelete" = false 
+        AND e."isActive" = true 
+        AND e."statusId" != 3
+        AND e."startTime"::date BETWEEN CURRENT_DATE + INTERVAL '1 day' AND CURRENT_DATE + INTERVAL '2 day'
+	    GROUP BY e.id, b.mail
+    `);
+
+    const events = result.rows;
+
+    for (const event of events) {
+      const { name, startTime, mail } = event;
+
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+          user: 'eventhub173@gmail.com', 
+          pass: 'jprz kvkb ncra wflf',  
+        },
+      });
+    
+      const mailOptions = {
+        from: 'eventhub173@gmail.com',
+        to: mail,
+        subject: `Reminder: Event "${name}" is happening tomorrow!`,
+        html: `Dear customer,\n\n
+                    This is a reminder that the event "${name}" you registered for will take place tomorrow at ${startTime}.\n\n
+                    We look forward to seeing you there!`,
+      };
+    
+      await transporter.sendMail(mailOptions);
+    }
+
+    console.log('Emails sent successfully');
+  } catch (err) {
+    console.error('Error sending event reminders:', err);
+  }
+});
 
 app.use(cors());
 
